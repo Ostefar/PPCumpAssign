9410b4be6c4055b564968277b4ffdc1f38b25d1f
Ostefar <56957832+Ostefar@users.noreply.github.com>
2022-10-03 15:22:31 +0200

    init setup

---
+++ PrimeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPCumpAssign
+{
+    public class PrimeGenerator
+    {
+        List<long> GetPrimesSequential(long first, long last)
+        {
+            var primes = new List<long>();
+
+            return primes;
+        }
+        List<long> GetPrimesParallel(long first, long last) 
+        {
+            var primes = new List<long>();
+
+            return primes;
+
+        }
+    }
+}
