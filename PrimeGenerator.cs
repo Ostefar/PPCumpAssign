cd9154c6ba828962a42cbba556d14ef54fe27874
Ostefar <56957832+Ostefar@users.noreply.github.com>
2022-10-04 21:30:13 +0200

    almost done
    sequential forloop style done
    sequential linq done
    parallel linq done
    listbox missing (dont know how to)

--- PrimeGenerator.cs
+++ PrimeGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,12 +10,15 @@ namespace primeGenerator
 {
     public class PrimeGenerator
     {
+        static readonly object theLock = new object();
+        static long index = -1;
         public static List<long> GetPrimesSequential(long first, long last)
         {
             var sw = Stopwatch.StartNew();
 
             List<long> primes = new List<long>();
 
+            Console.WriteLine("Sequential");
             Console.WriteLine($"The Prime Numbers between {first} and {last} are: ");
             for (long i = first; i <= last; i++)
             {
@@ -33,21 +37,70 @@ namespace primeGenerator
                     primes.Add(i);
                 }
             }
-            foreach (object prime in primes)
+            // remove commentation for printing primes
+            /*foreach (int prime in primes)
             {
                 Console.Write(prime + " ");
-            }
+            }*/
             sw.Stop();
             Console.WriteLine("");
             Console.WriteLine("Time = {0:f3} sec.", sw.ElapsedMilliseconds / 1000d);
+            Console.WriteLine("----------------------------------------------------");
             return primes;
         }
 
-        public static List<long> GetPrimesParallel(long first, long last) 
+        public static List<int> GetPrimesSequentialLINQ(int first, int last)
         {
-            List<long> primes = new List<long>();
+            var sw = Stopwatch.StartNew();
 
-            return primes;
+            IEnumerable<int> range = Enumerable.Range(first, last - first);
+
+            Console.WriteLine("Sequential LINQ");
+            //Console.WriteLine($"The Prime Numbers between {first} and {last} are: ");
+
+            List<int> primes = (from n in range
+                         let w = (int)Math.Sqrt(n)
+                         where Enumerable.Range(2, w).All((i) => n % i > 0)
+                         select n).ToList();
+
+            // remove commentation for printing primes
+            /*foreach (int prime in primes)
+            {
+                Console.Write(prime + " ");
+            }*/
+            sw.Stop();
+            Console.WriteLine("");
+            Console.WriteLine("Time = {0:f3} sec.", sw.ElapsedMilliseconds / 1000d);
+            Console.WriteLine("----------------------------------------------------");
+            return primes.ToList();
+
+        }
+        public static List<int> GetPrimesParallelLINQ(int first, int last) 
+            {
+            var sw = Stopwatch.StartNew();
+
+            Console.WriteLine("Parallel LINQ");
+            //Console.WriteLine($"The Prime Numbers between {first} and {last} are: ");
+
+            IEnumerable<int> range = Enumerable.Range(first, last);
+
+            List<int> primes = (from n in range.AsParallel()
+                          let w = (int)Math.Sqrt(n)
+                          where Enumerable.Range(2, w).All((i) => n % i > 0)
+                          select n).ToList();
+
+            primes.Sort();
+
+            // remove commentation for printing primes
+            /* foreach (int prime in primes)
+             {
+                 Console.Write(prime + " ");
+             }*/
+            sw.Stop();
+            Console.WriteLine("");
+            Console.WriteLine("Time = {0:f3} sec.", sw.ElapsedMilliseconds / 1000d);
+            Console.WriteLine("----------------------------------------------------");
+            return primes.ToList();
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -7,13 +7,27 @@ namespace PPCumpAssign
         static void Main(string[] args)
 
         {
-            RunOrder();
+            bool isRunning = true;
+            do
+            {
+                Console.WriteLine("Welcome to the prime generator - x to escape");
+                Console.WriteLine("Enter range start");
+                var first = Console.ReadLine();
+                Console.WriteLine("Enter range end");
+                var last = Console.ReadLine();
+                Console.WriteLine("----------------------------------------------------");
+
+                RunOrder(Int32.Parse(first), Int32.Parse(last));
+
+            } while (isRunning == true);
         }
 
-        static void RunOrder()
+        static void RunOrder(int first, int last)
         {
-            PrimeGenerator.GetPrimesSequential(1000000,1000100);
-            PrimeGenerator.GetPrimesParallel(100,500);
+
+            //PrimeGenerator.GetPrimesSequential(first,last); // be aware, this will take a long time with big intervals (approx 250 seconds for 1-1000000)
+            PrimeGenerator.GetPrimesSequentialLINQ(first, last);
+            PrimeGenerator.GetPrimesParallelLINQ(first, last);
         }
 
     }
