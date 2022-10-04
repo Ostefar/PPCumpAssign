120fa55165e9862c7e7b434007afa374a3f2fb57
Ostefar <56957832+Ostefar@users.noreply.github.com>
2022-10-04 22:02:14 +0200

    done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace PPCumpAssign
     {
         // INFO: at this moment, the printing of the primes is commented out in PrimeGenerator.class, in each of the functions.
         // This is done on purpose, to easier see the test results instead of millions of numbers. Remove the commentation, if you want too see the primes
+        // Instead I've added a counter, so you can see that the same amount of primes is getting added in the different functions
 
+        // Still to be implemented:
+        // Add results to ListBox - I've never worked with it before, and wanted to add it by the end. But it seems i must rework quite a lot
+        // to implement this feature, that i kinda made myself. So i've decided to go without, and use my "own".
         static void Main(string[] args)
         {
             bool isRunning = true;
@@ -26,7 +30,7 @@ namespace PPCumpAssign
 
         static void RunOrder(int first, int last)
         {
-
+            //If you value your spare time, please do not remove comments from the next line
             //PrimeGenerator.GetPrimesSequential(first,last); // be aware, this will take a long time with big intervals (approx 250 seconds for 1-1000000)
             PrimeGenerator.GetPrimesSequentialLINQ(first, last);
             PrimeGenerator.GetPrimesParallelLINQ(first, last);
